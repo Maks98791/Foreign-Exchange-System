eef0cd52fcbafb612aba88082db90ded464c4595
Kasiek <frygtt@gmail.com>
2020-04-08 13:48:56 +0200

    Dodanie strony wallet powbierajacej dane zalogowanego użytkownika

---
+++ src/main/java/com/example/ForeignExchangeSystem/controller/WalletController.java
@@ -0,0 +1,29 @@
+package com.example.ForeignExchangeSystem.controller;
+
+import com.example.ForeignExchangeSystem.Service.UserService;
+import com.example.ForeignExchangeSystem.model.User;
+import com.example.ForeignExchangeSystem.shared.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WalletController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/wallet")
+    public String Wallet(Model model) {
+        String username = Utils.getLoggedUser();
+        User user = userService.findUserByMail(username);
+        int role_num = user.getRoles().iterator().next().getId();
+        user.setRole_num(role_num);
+        model.addAttribute("user", user);
+
+        return "wallet";
+    }
+
+}
