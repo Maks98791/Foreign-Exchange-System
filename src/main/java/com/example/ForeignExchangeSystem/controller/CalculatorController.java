60390176a3c67b771c8a362fbdf9ca791e713a43
navotan <krzysztofxjaros@gmail.com>
2020-04-23 18:04:52 +0200

    Dodany kalkulator walut

---
+++ src/main/java/com/example/ForeignExchangeSystem/controller/CalculatorController.java
@@ -0,0 +1,21 @@
+package com.example.ForeignExchangeSystem.controller;
+
+import com.example.ForeignExchangeSystem.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CalculatorController {
+
+
+    @GetMapping("/calculator")
+    public String Login() {
+        return "calculator";
+    }
+
+}
